1d8985dd280c147ef3e18354bb09686a75faf1cd
EllenSt264 <stafford.ellen@protonmail.com>
2021-08-23 14:12:25 +0100

    Add fixtures and models for Games app

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Game, Edition, Section, Content
 
-# Register your models here.
+
+class EditionInlineAdmin(admin.TabularInline):
+    model = Edition
+    extra = 0
+
+
+class SectionInlineAdmin(admin.TabularInline):
+    model = Section
+    extra = 0
+
+
+class ContentInlineAdmin(admin.TabularInline):
+    model = Content
+    extra = 0
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'pk',
+        'publisher',
+        'developer',
+        'date_added',
+    )
+
+    inlines = (
+        EditionInlineAdmin,
+        SectionInlineAdmin,
+        ContentInlineAdmin,
+    )
+
+    readonly_fields = ('date_added',)
+
+    fields = (
+        'friendly_name', 'name', 'publisher',
+        'publisher_friendly_name', 'developer',
+        'description', 'image_url', 'image',
+        'date_added',
+    )
+
+    ordering = (
+        'pk',
+    )
+
+
+admin.site.register(Game, GameAdmin)
--- games/models.py
+++ games/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+""" Creating classes for fixtures so that Django can use them """
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    publisher = models.CharField(max_length=254)
+    publisher_friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    developer = models.CharField(max_length=254)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    date_added = models.DateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.friendly_name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Edition(models.Model):
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name_full = models.CharField(max_length=254, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.friendly_name
+
+
+class Section(models.Model):
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name_extended = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.friendly_name
+
+
+class Content(models.Model):
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    edition = models.ForeignKey('Edition', on_delete=models.CASCADE)
+    section = models.ForeignKey('Section', null=True, blank=True, on_delete=models.SET_NULL)
+
+    content = models.CharField(max_length=254)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.content
