4f13cfd3c7ad912df507287b9ef8f8d41c835dae
EllenSt264 <stafford.ellen@protonmail.com>
2021-08-23 14:35:54 +0100

    Add game data to games page

--- games/views.py
+++ games/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Game, Edition, Section, Content
 
 
 def all_games(request):
     """ A view to render all games available for purchase """
 
-    return render(request, 'games/games.html')
+    games = Game.objects.all()
+
+    template = 'games/games.html'
+    context = {
+        'games': games,
+    }
+
+    return render(request, template, context)
