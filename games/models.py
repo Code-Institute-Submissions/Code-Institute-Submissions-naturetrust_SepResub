26d774741edc6c2ee9a802bd5e5ca2bff2d5a7d8
EllenSt264 <stafford.ellen@protonmail.com>
2021-08-24 23:39:20 +0100

    Update models and fixtures for Games app

--- games/models.py
+++ games/models.py
@@ -10,7 +10,11 @@ class Game(models.Model):
     url_name = models.CharField(max_length=254, null=True, blank=True)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     publisher = models.CharField(max_length=254)
-    publisher_friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    publisher_friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
     developer = models.CharField(max_length=254)
     description = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -29,7 +33,13 @@ class Edition(models.Model):
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-    friendly_name_full = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name_full = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
+    desc = models.TextField(null=True, blank=True)
+    helper_text = models.CharField(max_length=254, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
@@ -41,7 +51,11 @@ class Section(models.Model):
 
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-    friendly_name_extended = models.CharField(max_length=254, null=True, blank=True)
+    friendly_name_extended = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.friendly_name
@@ -50,7 +64,12 @@ class Section(models.Model):
 class Content(models.Model):
     game = models.ForeignKey('Game', on_delete=models.CASCADE)
     edition = models.ForeignKey('Edition', on_delete=models.CASCADE)
-    section = models.ForeignKey('Section', null=True, blank=True, on_delete=models.SET_NULL)
+    section = models.ForeignKey(
+        'Section',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
 
     content = models.CharField(max_length=254)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
