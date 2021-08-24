a80ce7fbec86e9d6c28512b79347f1026f73326d
EllenSt264 <stafford.ellen@protonmail.com>
2021-08-24 17:07:30 +0100

    Add Game Details views and template

--- games/urls.py
+++ games/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.urls.resolvers import URLPattern
 from . import views
 
 
 urlpatterns = [
     path('', views.all_games, name='games'),
+    path('<game_title>/', views.game_details, name='game_details'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Game, Edition, Section, Content
 
 
@@ -10,6 +10,25 @@ def all_games(request):
     template = 'games/games.html'
     context = {
         'games': games,
+    }
+
+    return render(request, template, context)
+
+
+def game_details(request, game_title):
+    """ A view to show individual game details """
+
+    game = get_object_or_404(Game, url_name=game_title)
+    editions = Edition.objects.filter(game=game)
+    sections = Section.objects.filter(game=game)
+    content = Content.objects.filter(game=game)
+
+    template = 'games/game-details.html'
+    context = {
+        'game': game,
+        'editions': editions,
+        'sections': sections,
+        'content': content,
     }
 
     return render(request, template, context)
