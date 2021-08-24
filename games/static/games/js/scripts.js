a67a743e985438a0579fca11a63b6bb1300a196f
EllenSt264 <stafford.ellen@protonmail.com>
2021-08-24 23:09:27 +0100

    Update change edition JS function on games details page

--- games/static/games/js/scripts.js
+++ games/static/games/js/scripts.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
-
     /* Hide section headers that do not have content */
 
-    var sectionContent = $('.active .section-content');
-    for (i=0; i < sectionContent.length; i++) {
-        var findString = $(sectionContent[i]).text().match(/[abcdefghijklmnopqrstuvwxyz]/g)
-        if (!findString) {
-            $(sectionContent[i]).prev('.section-header').addClass('hide');
+    function hideEmptyHeaders() {
+        var sectionContent = $('.active .section-content');
+        for (i=0; i < sectionContent.length; i++) {
+            var findString = $(sectionContent[i]).text().match(/[abcdefghijklmnopqrstuvwxyz]/g)
+            if (!findString) {
+                console.log('empty', sectionContent[i])
+                $(sectionContent[i]).prev('.section-header').addClass('hide');
+            };
         };
-    };
+    }
+    hideEmptyHeaders(); 
 
     /* Change edition details section on edition button click */
 
@@ -19,5 +22,6 @@ $(document).ready(function() {
 
         $('.edition-details.active').removeClass('active').addClass('hide');
         $(`#editionDetails${editionId}`).removeClass('hide').addClass('active');
+        hideEmptyHeaders();
     });
 });
