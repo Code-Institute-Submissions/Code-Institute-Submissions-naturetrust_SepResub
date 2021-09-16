aaac0f5e8c4d78d8dcd684dc729a94c7ebace050
EllenSt264 <stafford.ellen@protonmail.com>
2021-09-16 17:17:13 +0100

    Add views, urls and template for Adoption app

--- adoption/views.py
+++ adoption/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Adoption
 
-# Create your views here.
+
+def all_adoptions(request):
+    """ A view to render all adoption packs
+    available to view and/or purchase """
+
+    adoptions = Adoption.objects.all()
+
+    template = 'adoption/adoptions.html'
+    context = {
+        'adoptions': adoptions,
+    }
+
+    return render(request, template, context)
--- naturetrust/urls.py
+++ naturetrust/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('games/', include('games.urls')),
+    path('adoption/', include('adoption.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
