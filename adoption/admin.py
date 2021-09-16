0666c62e348eb17a47f7513b99a787e7df31d7ba
EllenSt264 <stafford.ellen@protonmail.com>
2021-09-16 14:30:15 +0100

    Add fixtures and models for Adoption app

--- adoption/admin.py
+++ adoption/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Adoption
 
-# Register your models here.
+
+class AdoptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'animal',
+        'animal_plural',
+        'date_added',
+    )
+
+    readonly_fields = ('date_added',)
+
+    fields = (
+        'animal', 'animal_plural', 'desc',
+        'image_url', 'image', 'date_added',
+    )
+
+    ordering = (
+        'pk',
+    )
+
+
+admin.site.register(Adoption, AdoptionAdmin)
--- adoption/models.py
+++ adoption/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+""" Creating classes for fixtures so that Django can use them """
+
+
+class Adoption(models.Model):
+    animal = models.CharField(max_length=254)
+    animal_plural = models.CharField(max_length=254)
+    desc = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    date_added = models.DateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.animal_plural
