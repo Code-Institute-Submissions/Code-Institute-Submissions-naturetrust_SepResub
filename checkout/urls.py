b281025df8ab331d587db8fb04ac88083dd6935c
EllenSt264 <stafford.ellen@protonmail.com>
2021-09-22 15:35:58 +0100

    Add webhook handler to Checkout app for Stripe payments

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
 from django.urls.resolvers import URLPattern
 from . import views
+from .webhooks import webhook
 
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
