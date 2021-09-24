7d6b3470fb74b687d1acf3e5eb9542c1444f9860
EllenSt264 <stafford.ellen@protonmail.com>
2021-09-24 01:00:11 +0100

    Fix email functionality and update verification pages

--- naturetrust/settings.py
+++ naturetrust/settings.py
@@ -214,12 +214,12 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # Send emails
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'naturetrust@example.com'
+    DEFAULT_FROM_EMAIL = 'support@naturetrust.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smpt.gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    EMAIL_HOST_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
